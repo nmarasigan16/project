11597ee58e77b44da791c493b4123757b7667969
Michael Dong <michaeldong21@foxmail.com>
2015-10-17 15:54:01 -0500

    websocket simple setup

--- visualization/backend/server.js
+++ visualization/backend/server.js
@@ -1,12 +1,26 @@
 var express = require('express')
 var mongoose = require('mongoose')
 var router_base = require('./routes/base')
-var router_realtime_update = require('./routes/reatime_update');
-
+var router_realtime_update = require('./routes/realtime_update');
 var app = express()
+var server = require('http').Server(app);
+
+var sockets = [];
+
+app.use('/', express.static('../frontend'))
 
 app.use('/base', router_base)
 app.use('/realtime', router_realtime_update)
-// /realtime
+//realtime
 
-app.listen(8089)
+var io = require('socket.io')(server)
+
+io.on('connection', function(socket){
+	sockets.push(socket)
+	console.log('a user connected');
+});
+
+//Not app.listen
+server.listen(8089, "127.0.0.1", function(){
+	console.log("Started");
+})
